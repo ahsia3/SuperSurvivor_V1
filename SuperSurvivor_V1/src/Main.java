d1a3b4280d873cf705229b8ecf94a0ec3eeb7704
ahsia3 <ahsia3@student.gsu.edu>
2018-12-01 14:07:05 -0500

    created View

--- SuperSurvivor_V1/src/Main.java
+++ SuperSurvivor_V1/src/Main.java
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        View view = new View();
+        view.setVisible(true);
     }
     
 }
